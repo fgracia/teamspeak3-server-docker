e83301ce06c52d42d48ac1749c82aa19b56f3f40
Frederic Gracia <gracia.frederic@gmail.com>
2015-04-25 16:21:57 +0200

    Added a case in startup.sh

--- startup.sh
+++ startup.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-
 # Checking if files exist to make sure we're backing up the database to data
 if [ ! -f /opt/teamspeak/ts3server.sqlitedb ] && [ -f /data/ts3server.sqlitedb ]
 then
     ln -s /data/ts3server.sqlitedb /opt/teamspeak/ts3server.sqlitedb
 fi
     
-if [ -f /opt/teamspeak/ts3server.sqlitedb ] && [ ! -f /data/ts3server.sqlitedb ]
+if [ -f /opt/teamspeak/ts3server.sqlitedb ]
 then
 	mv /opt/teamspeak/ts3server.sqlitedb /data/ts3server.sqlitedb
     ln -s /data/ts3server.sqlitedb /opt/teamspeak/ts3server.sqlitedb
@@ -18,7 +17,12 @@ then
 	rm /opt/teamspeak/ts3server.sqlitedb
 	ln -s /data/ts3server.sqlitedb /opt/teamspeak/ts3server.sqlitedb
 fi
-                    
+
+if [ ! -f /opt/teamspeak/ts3server.sqlitedb ] && [ ! -f /data/ts3server.sqlitedb ]
+then
+	touch /data/ts3server.sqlitedb
+    ln -s /data/ts3server.sqlitedb /opt/teamspeak/ts3server.sqlitedb
+fi             
                     
 # Run the teamspeak server
 export LD_LIBRARY_PATH=/opt/teamspeak
