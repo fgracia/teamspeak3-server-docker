4cd10652187cd34a0a196567456aaa610625109f
Frédéric GRACIA <fgracia@sqli.com>
2018-09-21 18:40:55 +0200

    Updated for TS3 new version, added auto accept license option

--- startup.sh
+++ startup.sh
@@ -5,13 +5,13 @@ if [ ! -f /opt/teamspeak/ts3server.sqlitedb ] && [ -f /data/ts3server.sqlitedb ]
 then
     ln -s /data/ts3server.sqlitedb /opt/teamspeak/ts3server.sqlitedb
 fi
-    
+
 if [ -f /opt/teamspeak/ts3server.sqlitedb ]
 then
     mv /opt/teamspeak/ts3server.sqlitedb /data/ts3server.sqlitedb
     ln -s /data/ts3server.sqlitedb /opt/teamspeak/ts3server.sqlitedb
 fi
-            
+
 if [ -f /opt/teamspeak/ts3server.sqlitedb ] && [ -f /data/ts3server.sqlitedb ]
 then
     rm /opt/teamspeak/ts3server.sqlitedb
@@ -22,9 +22,9 @@ if [ ! -f /opt/teamspeak/ts3server.sqlitedb ] && [ ! -f /data/ts3server.sqlitedb
 then
     touch /data/ts3server.sqlitedb
     ln -s /data/ts3server.sqlitedb /opt/teamspeak/ts3server.sqlitedb
-fi             
-                    
+fi
+
 # Run the teamspeak server
 export LD_LIBRARY_PATH=/opt/teamspeak
 cd /opt/teamspeak
-./ts3server_linux_amd64 logpath=/data/logs/
+./ts3server logpath=/data/logs/ license_accepted=1
